75b35666cc1ad2315a626f89aefb6fbdc44ba878
Sangmin Park <sangmins930@gmail.com>
2024-04-19 16:52:45 +0900

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.springframework.boot") version "3.2.5"
     id("io.spring.dependency-management") version "1.1.4"
     id("org.sonarqube") version "4.4.1.3373"
+    id("jacoco")
 }
 
 group = "com.example"
